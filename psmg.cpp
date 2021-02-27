7f1129e1e3d8e9ce6d11f14525226fe0dc60b550
szf75 <60915279+szf75@users.noreply.github.com>
2021-02-27 18:58:55 +0500

    Create psmg.cpp

---
+++ psmg.cpp
@@ -0,0 +1,79 @@
+// password  manager-generator. psmg
+// for example 1. Input number spacebar word
+// 7 hello
+// input number: 7
+// input word:   hello or Hello or HELLO
+// output:       z6$4e2r5`6o7
+// chek difference
+// for example 2. Input only word
+// input: hello or Hello or HELLO
+// output: v7e085j1o3
+// for example 3. 
+// 7hello
+// input: 7hello or 7Hello or 7HELLO
+// output: z6e5p2r5o1 
+
+//#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{ 
+    unsigned rand_value;
+    cin >> rand_value;
+    srand(rand_value);
+    
+    int c, n;
+
+    while ((c = getchar()) != EOF ){
+         
+         if ((c%2) == 0) { // try !0
+           c = (33 + rand() % (126 - 33 +1));  
+         }; 
+         
+        n = (48 + rand() % (57 - 48));
+        putchar(c);
+        putchar(n);
+       // printf("%c", c);
+    }
+   // system("pause");
+    return 0;
+}
+
+/* man psmg
+How to use it. 
+1.Compile this code.  
+2. Guess a number, which will be manage and generate your paswords. For example: date or some numbers from your telephone number. Let it be 1980. That will be your secure number in your mind for all yours paswords!
+3. Write down in text file your words for your
+paswords. For example: mygmailbox1, Mygithub...
+mygmailbox1
+Mygithub...
+4. Run psmg in linux console or on your smartphone
+by Termux console. Example: ./psmg
+Enter: 1980mygmailbox1
+Output: m4y8g5m8aOiOr6W8O110
+
+to quit press Ctrl-D
+
+Important: at first enter your secure number and then word in one line. See example.
+Password generated only by word without secure number is not save.
+
+
+5. NO need to ramamber in your mind or write down this password in file. Write down is not secure.
+Dont copy-paste by buffer (no secure).
+Dont use Screeshot or Printscreen (no secure).
+Better way enter manualy. You can generate password on smartphone and enter it on your computer browser where you login. Or reverse.
+Or write down on paper. After that you should eat it or store in save place.
+
+REMEMBER if you use psmg on smartphone and on computer then passwords will be generated DIFFERENT at the same secure number and words! Because different architecture and OS.
+
+Use psmg only on smarphone or only on computer.
+Better way use psmg on smartphone and store passwords on paper in save place.
+
+6. Every time when you need a password you
+chek your words in text file and ramamber your secure number. After that run psmg and generate your password.
+Thus passwords dont exist in your system on your computer or smartphone. Exist only words on your computer.
+And exist secure number in your head.
+
+*/
+
